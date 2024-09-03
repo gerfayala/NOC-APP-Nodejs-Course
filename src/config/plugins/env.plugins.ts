6d05496a294f085877a85eb5efa51ae50bd831c3
German <germanfayala2419@gmail.com>
2024-09-03 13:06:33 -0400

    feat/Add-ENV: added env configuration

---
+++ src/config/plugins/env.plugins.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+
+import * as env from 'env-var';
+
+
+export const envs = {
+    PORT: env.get('PORT').required().asPortNumber(),
+    MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
+    MAILER_PASSWORD: env.get('MAILER_PASSWORD').required().asString(),
+    PROD: env.get('PROD').required().asBool(),
+}
